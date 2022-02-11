24ff68736052cc36e779867f29f8d709261e0ba8
Filou <philipperolet@gmail.com>
2022-02-11 17:38:35 +0100

    noop: index-new for later work on responsivity

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject mzero-game-gui "1.0.3"
+(defproject mzero-game-gui "1.0.4-alpha"
   :description "This is a ClojureScript GUI for the [Mzero Game](https://github.com/sittingbull/mzero-game)."
   :url "https://github.com/sittingbull/mzero-game-gui"
   :license {:name "Eclipse Public License"
