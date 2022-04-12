23fbcc99afae2bb67a8d88bd233577f964c89602
Filou <philipperolet@gmail.com>
2022-04-12 17:56:15 +0200

    ops: automated server shutdown after 1h

--- start-server-prod.sh
+++ start-server-prod.sh
@@ -2,5 +2,5 @@
 install_dir="/home/ubuntu/the-rabbit-game"
 cd $install_dir
 lein run -m claby.ux.server/serve 2>> /home/ubuntu/the-rabbit-game-server.err >> /home/ubuntu/the-rabbit-game-server.log </dev/null &
-sudo shutdown -h +120
+sudo shutdown -h +60
 
